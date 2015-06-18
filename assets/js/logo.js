a613a281a4ac139099bc791d633b4bf082a5146d
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-06-17 20:00:04 -0700

    refactor logo code

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -7,58 +7,30 @@ var svg = d3.select("#top-logo")
 
 var logo = svg.append("g");
 
-var rect = logo.append("rect")
-               .attr("x", 0)
-               .attr("y", 0)
-               .attr("width", 75)
-               .attr("height", 75)
-               .style("fill", "#fff");
+var x = 0,
+    y = 0,
+    width = 75,
+    height = 75;
 
-var smallerRect = logo.append("rect")
-                      .attr("x", 5)
-                      .attr("y", 5)
-                      .attr("width", 65)
-                      .attr("height", 65)
-                      .style("fill", "#000");
+while (x <= 35) {
+  if (x % 10 === 0) {
+    var rect = logo.append("rect")
+                   .attr("x", x)
+                   .attr("y", y)
+                   .attr("width", width)
+                   .attr("height", height)
+                   .style("fill", "#fff");
+  } else {
+    var rect = logo.append("rect")
+                   .attr("x", x)
+                   .attr("y", y)
+                   .attr("width", width)
+                   .attr("height", height)
+                   .style("fill", "#000");
+  }
 
-var smallerRect2 = logo.append("rect")
-                       .attr("x", 10)
-                       .attr("y", 10)
-                       .attr("width", 55)
-                       .attr("height", 55)
-                       .style("fill", "#fff");
-
-var smallerRect3 = logo.append("rect")
-                       .attr("x", 15)
-                       .attr("y", 15)
-                       .attr("width", 45)
-                       .attr("height", 45)
-                       .style("fill", "#000");
-
-var smallerRect4 = logo.append("rect")
-                       .attr("x", 20)
-                       .attr("y", 20)
-                       .attr("width", 35)
-                       .attr("height", 35)
-                       .style("fill", "#fff");
-
-var smallerRect5 = logo.append("rect")
-                       .attr("x", 25)
-                       .attr("y", 25)
-                       .attr("width", 25)
-                       .attr("height", 25)
-                       .style("fill", "#000");
-
-var smallerRect6 = logo.append("rect")
-                       .attr("x", 30)
-                       .attr("y", 30)
-                       .attr("width", 15)
-                       .attr("height", 15)
-                       .style("fill", "#fff");
-
-var smallerRect7 = logo.append("rect")
-                       .attr("x", 35)
-                       .attr("y", 35)
-                       .attr("width", 5)
-                       .attr("height", 5)
-                       .style("fill", "#000");
+  x += 5;
+  y += 5;
+  width -= 10;
+  height -= 10;
+}
