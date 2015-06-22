fb84901ccd983d440445ab988aa60c49291e8271
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-06-21 21:46:55 -0700

    check out viewport + html doc widths x heights

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 (function() {
+
   var $photo = $("#foto");
   $photo.click(function() {
     $photo.addClass("zoom");
-  })
+  });
+
+  var $browserViewportHeight = $(window).height();
+  var $browserViewportWidth = $(window).width();
+  console.log("browser viewport width x height: " + $browserViewportWidth + " x " + $browserViewportHeight);
+  var $htmlDocHeight = $(document).height();
+  var $htmlDocWidth = $(document).width();
+  console.log("HTML doc width x height: " + $htmlDocWidth + " x " + $browserViewportWidth);
+
 })();
